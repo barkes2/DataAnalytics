3427aa80e5832b2cdbcef7b3deb25e17b9358eb1
Sebastian Barkett <barkes2@rpi.edu>
2021-02-18 15:40:27 -0500

    Add files via upload

--- Assignment2_Lab2_Part1_BARKETT.R
+++ Assignment2_Lab2_Part1_BARKETT.R
@@ -1,7 +1,39 @@
 #useful commands here: hear(<object>); tail (<object>); summary(<object>)
+
 #Measures of Central Tendency for the EPI dataset:
 # 1) Generate Central Tendency values for EPI variable
+getwd()
+EPI_data<-read.csv("EPI_data.csv")#reads in the csv file from the pathway you oultine. check from "getwd()"
+attach(EPI_data) #sets the default object
+View(EPI_data) #opens window with EPI data
+fix(EPI_data) #opens editing window
+EPI #displays values from the EPI column in the EPI_data; lots of NA values to be masked
+tf <- is.na(EPI) #records true value as NA
+E<-EPI[!tf] #filters out these NA values making a new array
+E #just so you can view the new array and show that it is missing NA values
+summary(E) #displays stats about the EPI data in array without NA values
+fivenum(E,na.rm=TRUE) #similar to above: returns Tukey's five number summary for the data (min,lower hinge, median, upper-hinge, max)
+#values should be:
+#Min.   1st Qu.   Median    Mean    3rd Qu.   Max.
+#32.10  48.60     59.20     58.37   67.60     93.50
+
+#how do we find the mode?
+# a)create a function
+mode<-function(v) {
+  uniqv<-(v)
+  uniqv[which.max(tabulate(match(v,uniqv)))]
+}
+# b) create the vector with numbers
+v<-EPI[!tf]
+# c) calculate the mode using the user function
+result<-mode(v)
+print(result)
+#the mode of EPI is:
+#> print(result)
+#[1] 44.6
+
 # 2) Generate Central Tendency values for DALY variable
+
 #Generate Histograms for EPI and DALY variables in the EPI 2010 dataset
 # 1) Generate the Histogram for EPI variable
 # 2) Genreate the Histogram for the DALY variable
@@ -14,8 +46,6 @@ qqplot(ENVHEALTH,ECOSYSTEM)
 #Using the EPI (under /EPI on web) dataset find the single most important factor in increasing the EPI in a given region
 
 #Linear and least-squares
-EPI_data<-read.csv("EPI_data.csv")
-attach(EPI_data);
 boxplot(ENVHEALTH,DALY,AIR_H,WATER_H)
 lmENVH<-lm(ENVHEALTH~DALY+AIR_H+WATER_H)
 lmENVH
