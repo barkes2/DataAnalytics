9dc7397d57cebc64abef4fe6b11e8fec3f9a44e0
Sebastian Barkett <barkes2@rpi.edu>
2021-03-31 10:24:42 -0400

    Add files via upload

--- group4_coleman.R
+++ group4_coleman.R
@@ -4,6 +4,15 @@ library(robustbase)
 data(coleman)
 head(coleman)
 c<-call('lmrob',formula=Y~.)
+#set up folds for cross-validation
 folds<-cvFolds(nrow(coleman),K=5,R=10)
-cvTool(c,data=coleman,y-coleman$Y,cost=rtmspe,folds=folds,costArgs = list(trim=0.1))
+#perform cross validation
+cvTool(c,data=coleman,y-coleman$Y,cost=rtmspe,folds=folds,
+       costArgs = list(trim=0.1))
+#vary K and R
+#look at cvfits, use densityplot
 tuning<-list(tuning.psi=seq(2.,6.,20))
+cvFitsLmrob<-cvTuning(c,data=coleman$Y,tuning=tuning,cost=rtmspe,folds=folds,
+                      costArgs=list(trim=0.1))
+cvFitsLmrob#look at output
+aggregate(cvFitsLmrob,summary)#summarize
